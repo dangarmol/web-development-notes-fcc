34695fb8f66714a257f285cda67a1d65267f1e75
Daniel G <dangar13@icloud.com>
2020-04-11 14:54:46 +0200

    Started React chapter

---
+++ Front End Libraries Certification/React/create-a-complex-jsx-element.jsx
@@ -0,0 +1,11 @@
+// write your code here
+const JSX = 
+(<div>
+  <h1>Title</h1>
+  <p>Paragraph</p>
+  <ul>
+    <li>Element 1</li>
+    <li>Element 2</li>
+    <li>Element 3</li>
+  </ul>
+</div>);
---
+++ Front End Libraries Certification/React/create-a-stateless-functional-component.jsx
@@ -0,0 +1,7 @@
+const MyComponent = function() {
+  // change code below this line
+
+  return (<div>I am a div</div>);
+
+  // change code above this line
+}
