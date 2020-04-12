7a1792f93954634a33818afe4485ccbf2a5dca91
Daniel G <dangar13@icloud.com>
2020-04-12 22:48:43 +0200

    Continue React chapter

---
+++ Front End Libraries Certification/React/create-a-controlled-form.jsx
@@ -0,0 +1,39 @@
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    // change code below this line
+    event.preventDefault();
+    this.setState(state => ({
+      submit: this.state.input
+    }));
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          { /* change code below this line */ }
+          <input type="text" onChange={this.handleChange} value={this.state.input}></input>
+          { /* change code above this line */ }
+          <button type='submit'>Submit!</button>
+        </form>
+        { /* change code below this line */ }
+        <h1>{this.state.submit}</h1>
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
