edf507dde5199ae7054d1d4ebeefa875c20ee425
Daniel G <dangar13@icloud.com>
2020-04-13 20:01:39 +0200

    Finished Redux chapter

---
+++ Front End Libraries Certification/Redux/copy-an-object-with-object.assign.js
@@ -0,0 +1,26 @@
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      // don't mutate state here or the tests will fail
+      let newObj = Object.assign({}, state);
+      newObj.status = "online";
+      return newObj;
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
