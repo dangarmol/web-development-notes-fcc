eb05d04a3f54de7a52a397e0ab9b4504cfe07150
Daniel G <dangar13@icloud.com>
2020-09-29 18:44:44 +0200

    Finished the Basic Node and Express module

---
+++ APIs and Microservices Certification/Basic Node and Express/7 - Implement a Root-Level Request Logger Middleware/myApp.js
@@ -0,0 +1,73 @@
+
+var express = require('express');
+var app = express();
+
+// --> 7)  Mount the Logger middleware here
+app.use(function(req, res, next) {
+   console.log(req.method + " " + req.path + " - " + req.ip);
+   next();
+ });
+
+// --> 11)  Mount the body-parser middleware  here
+
+
+/** 1) Meet the node console. */
+console.log("Hello World");
+
+/** 2) A first working Express Server */
+//app.get("/", function(req, res) {
+//   res.send("Hello Express");
+//});
+
+/** 3) Serve an HTML file */
+app.get("/", function(req, res) {
+   path = __dirname + "/views/index.html";
+   res.sendFile(path);
+});
+
+/** 4) Serve static assets  */
+app.use(express.static(__dirname + "/public"));
+
+/** 5) serve JSON on a specific route */
+//app.get("/json", function(req, res) {
+//   res.json({"message": "Hello json"});
+//});
+
+/** 6) Use the .env file to configure the app */
+app.get("/json", function(req, res) {
+   if(process.env.MESSAGE_STYLE == "uppercase") {
+      res.json({"message": "Hello json".toUpperCase()});
+   } else {
+      res.json({"message": "Hello json"});
+   }
+});
+ 
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
+
+/** 8) Chaining middleware. A Time server */
+
+
+/** 9)  Get input from client - Route parameters */
+
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+
+  
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+
+
+/** 12) Get data form POST  */
+
+
+
+// This would be part of the basic setup of an Express app
+// but to allow FCC to run tests, the server is already active
+/** app.listen(process.env.PORT || 3000 ); */
+
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
+
+ module.exports = app;
