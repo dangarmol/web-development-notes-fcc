6d90fe4980e6931f2a8200d98865805d0992ee09
Daniel G <dangar13@icloud.com>
2020-10-01 22:00:21 +0200

    Finished Header Parser project

--- APIs and Microservices Certification/APIs and Microservices Projects/Request Header Parser Microservice/server.js
+++ APIs and Microservices Certification/APIs and Microservices Projects/Request Header Parser Microservice/server.js
@@ -24,6 +24,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.get("Accept-Language"),
+    "software": req.get("User-Agent")
+  });
+});
 
 
 // listen for requests :)
