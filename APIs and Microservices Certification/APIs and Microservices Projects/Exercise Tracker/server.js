b77f26519b5eb14455c4820e0d087cc3fa936623
Daniel G <dangar13@icloud.com>
2020-10-06 23:56:14 +0200

    Added user stories and connection to MongoDB

--- APIs and Microservices Certification/APIs and Microservices Projects/Exercise Tracker/server.js
+++ APIs and Microservices Certification/APIs and Microservices Projects/Exercise Tracker/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors())
 
@@ -42,6 +42,107 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  username:  {type: String, required: true},
+  log:  [
+          {
+            description: String,
+            duration: Number,
+            date: Date
+          }
+        ]
+});
+
+var Person = mongoose.model("Person", personSchema);
+
+function personExistsByName(nameToSearch) {
+  Person.exists({ name: nameToSearch }, function(err, result) {
+    if (err) {
+      console.log(err);
+      return err;
+    } else {
+      return result;
+    }
+  });
+}
+
+/**
+ * Return:
+ * an object with username and _id
+    {
+      "username": "dandandan",
+      "_id": "5f7cbec6ebd65c0039866141"
+    }
+
+  If taken, return "Username already taken"
+*/
+app.post("/api/exercise/new-user", function (req, res) {
+  
+  if(personExistsByName(req.body.uname)) {
+    res.json({"error": "Username already exists!"});
+  } else {
+    // Insert into database
+    res.json({"username": req.body.uname, "_id": "testtesttest"});
+  }
+  
+});
+
+/**
+ * Return:
+ * App will return the user object with the exercise fields added.
+  {
+    "_id": "5f7cbf8febd65c0039866144",
+    "username": "dandandan2",
+    "date": "Sun Sep 06 2020",
+    "duration": 25,
+    "description": "Ejercicio 1"
+  }
+
+  Given:
+  Post with ID, exercise name, duration and (date). If no date supplied it will use current date.
+ */
+app.post("/api/exercise/add", function (req, res) {
+  
+  if(!savedURLs.includes(req.body.url)) {
+    savedURLs[nextURL] = req.body.url;
+    nextURL++; 
+  }
+
+  res.json({"original_url": req.body.url, "short_url": savedURLs.indexOf(req.body.url)});
+  
+});
+
+/**
+ * Return:
+ * {
+    "_id": "5f7cbf8febd65c0039866144",
+    "username": "dandandan2",
+    "count": 2,
+    "log":
+    [
+      {
+        "description": "Ejercicio 1",
+        "duration": 25,
+        "date": "Sun Sep 06 2020"
+      },
+      {
+        "description": "Ejercicio 1",
+        "duration": 25,
+        "date": "Sun Sep 06 2020"
+      }
+    ]
+  }
+  Requires at least one ID in the params. Returns the count and the exercises. It also admits filters.
+ */
+app.get("/api/exercise/log", function(req, res) {
+
+  //req.query.first
+  res.redirect(savedURLs[req.params.identifier]);
+
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
