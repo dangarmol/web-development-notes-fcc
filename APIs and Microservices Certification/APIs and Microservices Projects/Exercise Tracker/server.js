9fdceb0d646d36df1fcc7c43b5a2eb33dd83d641
Daniel G <dangar13@icloud.com>
2020-11-01 00:22:43 +0100

    Exercise tracker modifications
    Connection was not working before due to wrong MongoDB and Mongoose npm versions!

--- APIs and Microservices Certification/APIs and Microservices Projects/Exercise Tracker/server.js
+++ APIs and Microservices Certification/APIs and Microservices Projects/Exercise Tracker/server.js
@@ -18,34 +18,9 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
-
 const Schema = mongoose.Schema;
 
-const personSchema = new Schema({
+const userSchema = new Schema({
   username:  {type: String, required: true},
   log:  [
           {
@@ -56,18 +31,7 @@ const personSchema = new Schema({
         ]
 });
 
-var Person = mongoose.model("Person", personSchema);
-
-function personExistsByName(nameToSearch) {
-  Person.exists({ name: nameToSearch }, function(err, result) {
-    if (err) {
-      console.log(err);
-      return err;
-    } else {
-      return result;
-    }
-  });
-}
+var User = mongoose.model("User", userSchema, "exercise-tracker");
 
 /**
  * Return:
@@ -79,13 +43,25 @@ function personExistsByName(nameToSearch) {
 
   If taken, return "Username already taken"
 */
-app.post("/api/exercise/new-user", function (req, res) {
+app.post("/api/exercise/new-user", async (req, res) => {
   
-  if(personExistsByName(req.body.uname)) {
-    res.json({"error": "Username already exists!"});
+  console.log("POST: Adding " + req.body.username + " to database...")
+
+  const user = await User.findOne({ "username": req.body.username });
+
+  if(user) {
+    res.send("Username already taken");
   } else {
-    // Insert into database
-    res.json({"username": req.body.uname, "_id": "testtesttest"});
+
+    const newUser = new User({
+      username: req.body.username
+    })
+
+    await newUser.save(function(err, data) {
+      if (err) return console.error(err);
+      res.json({"username": data.username, "_id": data.id});
+    });
+
   }
   
 });
@@ -104,14 +80,24 @@ app.post("/api/exercise/new-user", function (req, res) {
   Given:
   Post with ID, exercise name, duration and (date). If no date supplied it will use current date.
  */
-app.post("/api/exercise/add", function (req, res) {
+app.post("/api/exercise/add", async (req, res) => {
   
-  if(!savedURLs.includes(req.body.url)) {
-    savedURLs[nextURL] = req.body.url;
-    nextURL++; 
-  }
+  console.log("POST: Adding exercise " + req.body.description + " to database...")
 
-  res.json({"original_url": req.body.url, "short_url": savedURLs.indexOf(req.body.url)});
+  const user = await User.findOne({ "_id": req.body.userId });
+
+  if(!user) {
+    res.send("User ID not found");
+  } else {
+    const newLog = {"description": req.body.description, "duration": req.body.duration, "date": req.body.date};
+
+    User.update({ "_id": req.body.userId }, { $push: { log: newLog } }, {new: true}, function (err, data) {
+      if (err) return console.error(err);
+      const newAdded = data.log[data.log.length - 1];
+      res.json({"_id": data.id, "username": data.username, "date": newAdded.date, "duration": newAdded.duration, "description": newAdded.description});
+    });
+
+  }
   
 });
 
