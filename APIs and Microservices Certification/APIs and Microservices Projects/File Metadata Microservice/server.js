dc481fe8c6468adcd108c20d74bfdccb5c711f01
Daniel G <dangar13@icloud.com>
2020-11-02 21:33:11 +0100

    Finished File Metadata Microservice
    APIs and Microservices Certification finished! :D

--- APIs and Microservices Certification/APIs and Microservices Projects/File Metadata Microservice/server.js
+++ APIs and Microservices Certification/APIs and Microservices Projects/File Metadata Microservice/server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
 
 var app = express();
 
@@ -20,4 +21,10 @@ app.get('/hello', function(req, res){
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
+});
+
+const multerInstance = multer().single("upfile");
+
+app.post('/api/fileanalyse', multerInstance, function(req, res){
+  res.json({"name": req.file.originalname, "type": req.file.mimetype, "size": req.file.size});
 });
