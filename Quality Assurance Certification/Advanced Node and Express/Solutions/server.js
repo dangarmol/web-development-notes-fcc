6604f00e8bd0b418d6c307027c315b5bf2b0ccb3
Daniel G <dangar13@icloud.com>
2020-11-04 16:12:17 +0100

    Added Solutions folder and first exercise

---
+++ Quality Assurance Certification/Advanced Node and Express/Solutions/server.js
@@ -0,0 +1,21 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+const app = express();
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "pug");
+
+app.route('/').get((req, res) => {
+  res.render(process.cwd() + "/views/pug");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
+});
