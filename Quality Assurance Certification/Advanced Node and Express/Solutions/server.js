9361acd8c5cf0db77a039be225c8f9fddd77f119
Daniel G <dangar13@icloud.com>
2020-11-04 22:36:26 +0100

    Started the authentication part of the app using Passport.js

--- Quality Assurance Certification/Advanced Node and Express/Solutions/server.js
+++ Quality Assurance Certification/Advanced Node and Express/Solutions/server.js
@@ -1,8 +1,12 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 
@@ -12,9 +16,60 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + "/views/pug");
+app.use(session({
+  secret: process.env.SESSION_SECRET,  // Used to compute the hash used to encrypt the cookie.
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render(process.cwd() + '/views/pug/index', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true
+    });
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render(process.cwd() + '/views/pug/index', { title: e, message: 'Unable to login' });
+  });
 });
+
+//app.route('/').get((req, res) => {
+//  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+//});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT);
