c047cef18e918d59a20daadf62016f2ed911e182
Daniel G <dangar13@icloud.com>
2020-11-12 19:45:36 +0100

    Finished Quality Assurance lessons
    Some comments with notes should still be added

--- Quality Assurance Certification/Advanced Node and Express/Solutions/public/client.js
+++ Quality Assurance Certification/Advanced Node and Express/Solutions/public/client.js
@@ -1,18 +1,33 @@
 $(document).ready(function () {
   
   // io() works only when connecting to a socket hosted on the same url/server.
-  // For connecting to an external socket hosted elsewhere, you would use io.connect('URL');
+  // For connecting to an external socket hosted elsewhere, you would use io.connect("URL");
   let socket = io();
 
-  socket.on("user count", function(data) {
-    console.log(data);
+  // socket.on("user count", function(data) {
+  //   console.log(data);
+  // });
+
+  socket.on("user", (data) => {
+    $("#num-users").text(data.currentUsers + " users online");
+    const message = data.name + (data.connected ? " has joined the chat." : " has left the chat.");
+    $("#messages").append($("<li>").html("<b>" + message + "</b>"));
   });
 
-  // Form submittion with new message in field with id 'm'
-  $('form').submit(function () {
-    var messageToSend = $('#m').val();
+  socket.on("chat message", (data) => {
+    $("#num-users").text(data.currentUsers + " users online");
+    const message = data.name + ": " + data.message;
+    $("#messages").append($("<li>").html("<b>" + message + "</b>"));
+  });
 
-    $('#m').val('');
+  // Form submittion with new message in field with id "m"
+  $("form").submit(function () {
+    var messageToSend = $("#m").val();
+
+    // On the socket architecture, the socket.emit function sends a request, potentially with data to the server (or viceversa), and the socket.on function listens for events.
+    socket.emit("chat message", messageToSend);  // "chat message" is the name of the event and messageToSend is the data.
+
+    $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
 });
--- Quality Assurance Certification/Advanced Node and Express/Solutions/server.js
+++ Quality Assurance Certification/Advanced Node and Express/Solutions/server.js
@@ -8,9 +8,13 @@ const bcrypt = require("bcrypt");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const ObjectID = require("mongodb").ObjectID;
+const passportSocketIo = require("passport.socketio");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo")(session);
+const store = new MongoStore({ url: process.env.MONGO_URI });
 
-const routes = require("./routes.js");
-const auth = require("./auth.js");
+const routes = require("./routes");
+const auth = require("./auth");
 
 const app = express();
 
@@ -27,10 +31,24 @@ app.use(session({
   secret: process.env.SESSION_SECRET,  // Used to compute the hash used to encrypt the cookie.
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: "express.sid",
+  store: store
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: "express.sid",
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async client => {
   const myDataBase = await client.db("FreeCodeCamp").collection("users");
@@ -38,15 +56,34 @@ myDB(async client => {
   routes(app, myDataBase);
   auth(app, myDataBase);
 
+  let currentUsers = 0;
+
   io.on("connection", socket => {
     ++currentUsers;
-    io.emit("user count", currentUsers);
-    console.log("A user has connected");
+    //io.emit("user count", currentUsers);
+    console.log("User " + socket.request.user.name + " connected.");
+    io.emit("user", {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    });
 
     socket.on("disconnect", () => {
       --currentUsers;
-      io.emit("user count", currentUsers);
-      console.log("A user has disconnected");
+      //io.emit("user count", currentUsers);
+      console.log("User " + socket.request.user.name + " disconnected.");
+      io.emit("user", {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false
+      });
+    });
+
+    socket.on("chat message", (message) => {
+      io.emit("chat message", {
+        name: socket.request.user.name,
+        message: message
+      });
     });
   });
 
@@ -56,7 +93,17 @@ myDB(async client => {
   });
 });
 
-let currentUsers = 0;
+function onAuthorizeSuccess(data, accept) {
+  console.log("Successful connection to socket.io");
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log("Failed connection to socket.io:", message);
+  accept(null, false);
+}
 
 http.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
