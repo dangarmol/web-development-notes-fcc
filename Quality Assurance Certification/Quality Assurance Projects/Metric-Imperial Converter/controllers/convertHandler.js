27eedde7d649658b452a360cb024fc74564fce2b
Daniel G <dangar13@icloud.com>
2020-11-22 23:27:36 +0100

    Finished first version of the API

--- Quality Assurance Certification/Quality Assurance Projects/Metric-Imperial Converter/controllers/convertHandler.js
+++ Quality Assurance Certification/Quality Assurance Projects/Metric-Imperial Converter/controllers/convertHandler.js
@@ -1,50 +1,89 @@
-/*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
-
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    var result;
+    const regex = /[a-zA-Z]/;
+    const slicedInput = input.slice(0, input.search(regex));
+    let evaluatedInput = eval(slicedInput);
     
-    return result;
+    if(slicedInput.includes("//")) {
+      evaluatedInput = null;
+    }
+
+    if(slicedInput == "") {
+      return 1;
+    } else if(evaluatedInput == null || isNaN(evaluatedInput)) {
+      return "invalid";
+    } else {
+      return this.roundTo5(evaluatedInput);
+    }
   };
   
   this.getUnit = function(input) {
-    var result;
-    
-    return result;
+    const regex = /[a-zA-Z]/;
+    const units = ["gal", "l", "lbs", "kg", "mi", "km"];
+    const slicedInput = input.slice(input.search(regex)).toLowerCase();
+
+    if(units.includes(slicedInput)) {
+      return slicedInput;
+    } else {
+      return "invalid";
+    }
   };
   
   this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
+    const unitPairs = {
+      "gal": "l",
+      "l": "gal",
+      "lbs": "kg",
+      "kg": "lbs",
+      "mi": "km",
+      "km": "mi"
+    }
+
+    return unitPairs[initUnit]
   };
 
   this.spellOutUnit = function(unit) {
-    var result;
+    const units = {
+      "gal": "gallons",
+      "l": "liters",
+      "lbs": "pounds",
+      "kg": "kilograms",
+      "mi": "miles",
+      "km": "kilometers"
+    }
     
-    return result;
+    return units[unit.toLowerCase()];
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
+
+    const unitToFactor = {
+      "gal": galToL,
+      "l": (1 / galToL),
+      "lbs": lbsToKg,
+      "kg": (1 / lbsToKg),
+      "mi": miToKm,
+      "km": (1 / miToKm)
+    }
     
-    return result;
+    return this.roundTo5(unitToFactor[initUnit] * initNum);
   };
+
+  this.roundTo5 = function(number) {
+    const hasDecimals = number.toString().split(".").length > 1;
+    if (hasDecimals) {
+      return number.toString().split(".")[1].length <= 5 ? parseFloat(number) : parseFloat(number.toFixed(5));
+    } else {
+      return parseFloat(number);
+    }
+  }
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
+    return `${this.roundTo5(initNum)} ${this.spellOutUnit(initUnit)} converts to ${this.roundTo5(returnNum)} ${this.spellOutUnit(returnUnit)}`;
   };
   
 }
--- Quality Assurance Certification/Quality Assurance Projects/Metric-Imperial Converter/routes/api.js
+++ Quality Assurance Certification/Quality Assurance Projects/Metric-Imperial Converter/routes/api.js
@@ -20,11 +20,25 @@ module.exports = function (app) {
       var input = req.query.input;
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+
+      if(initNum != "invalid" && initUnit != "invalid") {
+        var returnNum = convertHandler.convert(initNum, initUnit);
+        var returnUnit = convertHandler.getReturnUnit(initUnit);
+        var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+        res.json({
+          "initNum": initNum,
+          "initUnit": initUnit,
+          "returnNum": returnNum,
+          "returnUnit": returnUnit,
+          "string": toString
+        })
+      } else if (initNum == "invalid" && initUnit == "invalid") {
+        res.json({"error": "invalid number and unit"})
+      } else if (initNum == "invalid") {
+        res.json({"error": "invalid number"})
+      } else {
+        res.json({"error": "invalid unit"})
+      }
     });
     
 };
