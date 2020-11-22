ed5b825a6b74047eff48c0b787e06f06acf588aa
Daniel G <dangar13@icloud.com>
2020-11-23 00:23:49 +0100

    Added functional tests

--- Quality Assurance Certification/Quality Assurance Projects/Metric-Imperial Converter/tests/2_functional-tests.js
+++ Quality Assurance Certification/Quality Assurance Projects/Metric-Imperial Converter/tests/2_functional-tests.js
@@ -26,31 +26,60 @@ suite('Functional Tests', function() {
         .end(function(err, res){
           assert.equal(res.status, 200);
           assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
+          assert.equal(res.body.initUnit, 'l'); // The returned units should always be lowercase according to the example!
           assert.approximately(res.body.returnNum, 2.64172, 0.1);
           assert.equal(res.body.returnUnit, 'gal');
           done();
         });
       });
       
-      test('Convert 32g (invalid input unit)', function(done) {
-        
-        //done();
+      test("Convert 32g (invalid input unit)", function(done) {
+        chai.request(server)
+        .get("/api/convert")
+        .query({input: "32g"})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, "invalid unit");
+          done();
+        });
       });
       
-      test('Convert 3/7.2/4kg (invalid number)', function(done) {
-        
-        //done();
+      // Testing for 3/7.2/4kg doesn't make sense as that can be a valid number.
+      // The example API also takes it as legal.
+      test("Convert 3//4kg (invalid number)", function(done) {
+        chai.request(server)
+        .get("/api/convert")
+        .query({input: "3//4kg"})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, "invalid number");
+          done();
+        });
       });  
       
-      test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
-        
-        //done();
+      test("Convert 3//4kilomegagram (invalid number and unit)", function(done) {
+        chai.request(server)
+        .get("/api/convert")
+        .query({input: "3//4kilomegagram"})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, "invalid number and unit");
+          done();
+        });
       });
       
-      test('Convert kg (no number)', function(done) {
-        
-        //done();
+      test("Convert kg (no number)", function(done) {
+        chai.request(server)
+        .get("/api/convert")
+        .query({input: "kg"})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 1);
+          assert.equal(res.body.initUnit, "kg"); // The returned units should always be lowercase according to the example!
+          assert.approximately(res.body.returnNum, 2.20462, 0.1);
+          assert.equal(res.body.returnUnit, "lbs");
+          done();
+        });
       });
       
     });
