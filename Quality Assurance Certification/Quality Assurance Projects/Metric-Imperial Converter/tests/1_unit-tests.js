2acc1ab8eed6b5542d8e9eb1627f67f5788353e7
Daniel G <dangar13@icloud.com>
2020-11-22 23:49:40 +0100

    Unit tests added

--- Quality Assurance Certification/Quality Assurance Projects/Metric-Imperial Converter/tests/1_unit-tests.js
+++ Quality Assurance Certification/Quality Assurance Projects/Metric-Imperial Converter/tests/1_unit-tests.js
@@ -17,34 +17,39 @@ suite('Unit Tests', function(){
   suite('Function convertHandler.getNum(input)', function() {
     
     test('Whole number input', function(done) {
-      var input = '32L';
+      var input = "32L";
       assert.equal(convertHandler.getNum(input),32);
       done();
     });
     
     test('Decimal Input', function(done) {
-      
-      //done();
+      var input = "32.123kg";
+      assert.equal(convertHandler.getNum(input),32.123);
+      done();
     });
     
     test('Fractional Input', function(done) {
-      
-      //done();
+      var input = "3/2mi";
+      assert.equal(convertHandler.getNum(input),1.5);
+      done();
     });
     
     test('Fractional Input w/ Decimal', function(done) {
-      
-      //done();
+      var input = "30.3/3km";
+      assert.equal(convertHandler.getNum(input),10.1);
+      done();
     });
     
     test('Invalid Input (double fraction)', function(done) {
-      
-      //done();
+      var input = "3//2L";
+      assert.equal(convertHandler.getNum(input),"invalid");
+      done();
     });
     
     test('No Numerical Input', function(done) {
-      
-      //done();
+      var input = "kg";
+      assert.equal(convertHandler.getNum(input),1);
+      done();
     }); 
     
   });
@@ -54,14 +59,16 @@ suite('Unit Tests', function(){
     test('For Each Valid Unit Inputs', function(done) {
       var input = ['gal','l','mi','km','lbs','kg','GAL','L','MI','KM','LBS','KG'];
       input.forEach(function(ele) {
-        //assert
+        const allowedValues = ['gal','l','mi','km','lbs','kg'];
+        assert.isTrue(allowedValues.includes(convertHandler.getUnit(ele)));
       });
       done();
     });
     
     test('Unknown Unit Input', function(done) {
-      
-      //done();
+      var input = "kog";
+      assert.equal(convertHandler.getUnit(input),"invalid");
+      done();
     });  
     
   });
@@ -82,7 +89,11 @@ suite('Unit Tests', function(){
   suite('Function convertHandler.spellOutUnit(unit)', function() {
     
     test('For Each Valid Unit Inputs', function(done) {
-      //see above example for hint
+      var input = ['gal','l','mi','km','lbs','kg'];
+      var expect = ['gallons','liters','miles','kilometers','pounds','kilograms'];
+      input.forEach(function(ele, i) {
+        assert.equal(convertHandler.spellOutUnit(ele), expect[i]);
+      });
       done();
     });
     
@@ -98,28 +109,38 @@ suite('Unit Tests', function(){
     });
     
     test('L to Gal', function(done) {
-      
-      //done();
+      var input = [50, 'l'];
+      var expected = 13.2086;
+      assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1); //0.1 tolerance
+      done();
     });
     
     test('Mi to Km', function(done) {
-      
-      //done();
+      var input = [100, 'mi'];
+      var expected = 160.934;
+      assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1); //0.1 tolerance
+      done();
     });
     
     test('Km to Mi', function(done) {
-      
-      //done();
+      var input = [40, 'km'];
+      var expected = 24.85490;
+      assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1); //0.1 tolerance
+      done();
     });
     
     test('Lbs to Kg', function(done) {
-      
-      //done();
+      var input = [25, 'lbs'];
+      var expected = 11.3398;
+      assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1); //0.1 tolerance
+      done();
     });
     
     test('Kg to Lbs', function(done) {
-      
-      //done();
+      var input = [50, 'kg'];
+      var expected = 110.23122;
+      assert.approximately(convertHandler.convert(input[0],input[1]),expected,0.1); //0.1 tolerance
+      done();
     });
     
   });
