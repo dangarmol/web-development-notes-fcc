5cff9b599998f72067f903b6b4f50cb040cfebd0
Daniel G <dangar13@icloud.com>
2020-11-23 22:59:40 +0100

    Added base code for Issue Tracker project

---
+++ Quality Assurance Certification/Quality Assurance Projects/Issue Tracker/routes/api.js
@@ -0,0 +1,35 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict';
+
+module.exports = function (app) {
+
+  app.route('/api/issues/:project')
+  
+    .get(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .post(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .put(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .delete(function (req, res){
+      let project = req.params.project;
+      
+    });
+    
+};
