2a0961861c9063f2a36854edb456418cdeab42aa
Daniel G <dangar13@icloud.com>
2020-11-28 22:44:09 +0100

    Finished unit tests

--- Quality Assurance Certification/Quality Assurance Projects/Sudoku Solver/tests/1_unit-tests.js
+++ Quality Assurance Certification/Quality Assurance Projects/Sudoku Solver/tests/1_unit-tests.js
@@ -1,11 +1,3 @@
-/*
- *
- *
- *       FILL IN EACH UNIT TEST BELOW COMPLETELY
- *       -----[Keep the tests in the same order!]----
- *       (if additional are added, keep them at the very end!)
- */
-
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -21,27 +13,27 @@ suite('UnitTests', () => {
   suite('Function validate()', () => {
     test('Valid Characters, length of 81', (done) => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-
-      // done();
+      assert.isTrue(solver.validatePuzzleLength(input));
+      done();
     });
 
     // Invalid characters or numbers are not accepted 
     // as valid input for the puzzle grid
     test('Invalid characters (anything other than "1-9" or "."") are not accepted', (done) => {
       const input = '..X..5.1.85.4....2432.HI...1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const error = "Invalid characters in puzzle";
-
-      // done();
+      //const error = "Invalid characters in puzzle";
+      assert.isFalse(solver.validatePuzzleCharacters(input));
+      done();
     });
 
     // Puzzles that are not 81 numbers/periods long show the message 
     test('Shows an error for puzzles that are not 81 numbers long', done => {
       const shortStr = '83.9.....6.62.71...9......1945....4.37.4.3..6..';
       const longStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...';
-      const errorMsg = 'Expected puzzle to be 81 characters long';
-      
-      
-      // done();
+      //const errorMsg = 'Expected puzzle to be 81 characters long';
+      assert.isFalse(solver.validatePuzzleLength(shortStr));
+      assert.isFalse(solver.validatePuzzleLength(longStr));
+      done();
     });
   });
   
@@ -51,8 +43,8 @@ suite('UnitTests', () => {
       const row = 0;
       const col = 0;
       const value = 3;
-      
-      // done();
+      assert.isTrue(solver.checkRowPlacement(input.split(""), row, value));
+      done();
     });
     
     test('Invalid placement for a row', done => {
@@ -60,8 +52,8 @@ suite('UnitTests', () => {
       const row = 0;
       const col = 0;
       const value = 9;
-
-      // done();
+      assert.isFalse(solver.checkRowPlacement(input.split(""), row, value));
+      done();
     });
     
   });
@@ -72,17 +64,17 @@ suite('UnitTests', () => {
       const row = 0;
       const col = 0;
       const value = 3;
-      
-      // done();
+      assert.isTrue(solver.checkColPlacement(input.split(""), col, value));
+      done();
     });
     
     test('Invalid placement for a column', done => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
       const row = 0;
       const col = 0;
-      const value = 9;
-
-      // done();
+      const value = 8;
+      assert.isFalse(solver.checkColPlacement(input.split(""), col, value));
+      done();
     });
     
   });
@@ -90,11 +82,11 @@ suite('UnitTests', () => {
   suite('Function checkRegionPlacement()', () => {
     test('Valid placement for a region', done => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const row = 4;
-      const col = 4;
-      const value = 3;
-      
-      // done();
+      const row = 0;
+      const col = 0;
+      const value = 7;
+      assert.isTrue(solver.checkRegionPlacement(input.split(""), row, col, value));
+      done();
     });
     
     test('Invalid placement for a region', done => {
@@ -102,8 +94,8 @@ suite('UnitTests', () => {
       const row = 4;
       const col = 4;
       const value = 7;
-
-      // done();
+      assert.isFalse(solver.checkRegionPlacement(input.split(""), row, col, value));
+      done();
     });
     
   });
@@ -111,23 +103,24 @@ suite('UnitTests', () => {
   suite('Function solvePuzzle()', () => {
     // Valid complete puzzles pass
     test('Valid puzzles pass', done => {
-      const input = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
-
-      // done();
+      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      assert.isString(solver.solveString(input));
+      done();
     });
 
     // Invalid complete puzzles fail
     test('Invalid puzzles fail', done => {
       const input = '779235418851496372432178956174569283395842761628713549283657194516924837947381625';
-
-      // done();
+      assert.isFalse(solver.solveString(input));
+      done();
     });
 
     // Returns the expected solution for a valid, incomplete puzzle
     test('Returns the expected solution for an incomplete puzzle', done => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
-      // done();
+      const solution = "769235418851496372432178956174569283395842761628713549283657194516924837947381625";
+      assert.equal(solver.solveString(input), solution);
+      done();
     });
   });
 });
