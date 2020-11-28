967d5e78cb05dc1131ed1bf6e1a837a3235b03de
Daniel G <dangar13@icloud.com>
2020-11-28 21:34:42 +0100

    Finished API exception checks

--- Quality Assurance Certification/Quality Assurance Projects/Sudoku Solver/routes/api.js
+++ Quality Assurance Certification/Quality Assurance Projects/Sudoku Solver/routes/api.js
@@ -13,7 +13,23 @@ module.exports = function (app) {
 
       console.log("POST on CHECK");
 
-      res.json(solver.checkPlacementJSON(req.body.puzzle, req.body.coordinate, req.body.value));
+      if (!req.body.hasOwnProperty("puzzle") || !req.body.hasOwnProperty("coordinate") || !req.body.hasOwnProperty("value")) {
+        res.json({"error": "Required field(s) missing"});
+      } else if (req.body.puzzle.length != 81) {
+        res.json({"error": "Expected puzzle to be 81 characters long"});
+      } else if (!req.body.coordinate.match(/^[A-I][1-9]$/gmi)) {
+        res.json({"error": "Invalid coordinate"});
+      } else if (isNaN(req.body.value) || req.body.value > 9 || req.body.value < 1) {
+        res.json({"error": "Invalid value"});
+      } else {
+        for (const char of req.body.puzzle) {
+          if (!["1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(char)) {
+            res.json({"error": "Invalid characters in puzzle"});
+            return;
+          }
+        }
+        res.json(solver.checkPlacementJSON(req.body.puzzle, req.body.coordinate, req.body.value));
+      }
     });
     
   app.route("/api/solve")
@@ -23,12 +39,23 @@ module.exports = function (app) {
 
       console.log("POST on SOLVE");
 
-      const solution = solver.solveString(req.body.puzzle);
-
-      if (solution) {
-        res.json({"solution": solution});
+      if (!req.body.hasOwnProperty("puzzle")) {
+        res.json({"error": "Required field missing"});
+      } else if (req.body.puzzle.length != 81) {
+        res.json({"error": "Expected puzzle to be 81 characters long"});
       } else {
-        res.json({"error": "Puzzle cannot be solved"});
+        for (const char of req.body.puzzle) {
+          if (!["1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(char)) {
+            res.json({"error": "Invalid characters in puzzle"});
+            return;
+          }
+        }
+        const solution = solver.solveString(req.body.puzzle);
+        if (solution) {
+          res.json({"solution": solution});
+        } else {
+          res.json({"error": "Puzzle cannot be solved"});
+        }
       }
     });
 };
