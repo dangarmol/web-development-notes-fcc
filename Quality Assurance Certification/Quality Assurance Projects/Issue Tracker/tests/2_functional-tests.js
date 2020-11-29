31c86f6cedbfaa6ec9d85fad7b73dda0b7e21bba
Daniel G <dangar13@icloud.com>
2020-11-30 00:10:16 +0100

    Added functional tests

--- Quality Assurance Certification/Quality Assurance Projects/Issue Tracker/tests/2_functional-tests.js
+++ Quality Assurance Certification/Quality Assurance Projects/Issue Tracker/tests/2_functional-tests.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
-
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
@@ -88,8 +80,21 @@ suite('Functional Tests', function() {
     });
     
     test('Missing required fields => { error: "required field(s) missing" }', function(done) {
+      chai.request(server)
+      .post('/api/issues/test')
+      .send({
+        issue_title: 'Title',
+        issue_text: 'text',
+        created_by: 'Functional Test - Every field filled in',
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        
+        assert.property(res.body, "error");
 
-      //done();
+        assert.equal(res.body.error, "required field(s) missing");
+        done();
+      });
     });
     
   });
@@ -117,13 +122,47 @@ suite('Functional Tests', function() {
     });
     
     test('One filter', function(done) {
-      
-      //done();
+      chai.request(server)
+      .get('/api/issues/test')
+      .query({_id: '5fb6e0c632a91870f0fa9dde'})
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        if (res.body.length > 0) {
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+        }
+        done();
+      });
     });
     
     test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
-      
-      //done();
+      chai.request(server)
+      .get('/api/issues/test')
+      .query({_id: '5fbecb782025bd03239f4d83', open: true, created_by: "Daniel"})
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        if (res.body.length > 0) {
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+        }
+        done();
+      });
     });
     
   });
@@ -131,28 +170,126 @@ suite('Functional Tests', function() {
   suite('PUT /api/issues/{project}', function() {
           
     test('One field to update => {result: "successfully updated", _id: _id}', function(done) {
-      
-      //done();
+      chai.request(server)
+      .put('/api/issues/test')
+      .send({
+        _id: '5fc4113e47c6e500b2053172',
+        issue_text: 'newText'
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        
+        assert.property(res.body, 'issue_title');
+        assert.property(res.body, 'issue_text');
+        assert.property(res.body, 'created_on');
+        assert.property(res.body, 'updated_on');
+        assert.property(res.body, 'created_by');
+        assert.property(res.body, 'assigned_to');
+        assert.property(res.body, 'open');
+        assert.property(res.body, 'status_text');
+        assert.property(res.body, '_id');
+
+        assert.equal(res.body.issue_text, "newText");
+      });
+      chai.request(server)
+      .get('/api/issues/test')
+      .query({_id: '5fc4113e47c6e500b2053172'})
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        if (res.body.length > 0) {
+          assert.property(res.body[0].issue_text, "newText");
+        }
+        done();
+      });
     });
     
     test('Multiple fields to update => {result: "successfully updated", _id: _id}', function(done) {
-      
-      //done();
+      chai.request(server)
+      .put('/api/issues/test')
+      .send({
+        _id: '5fc4113e47c6e500b2053172',
+        issue_text: 'NewNewTextFromPut',
+        open: false
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        
+        assert.property(res.body, 'issue_title');
+        assert.property(res.body, 'issue_text');
+        assert.property(res.body, 'created_on');
+        assert.property(res.body, 'updated_on');
+        assert.property(res.body, 'created_by');
+        assert.property(res.body, 'assigned_to');
+        assert.property(res.body, 'open');
+        assert.property(res.body, 'status_text');
+        assert.property(res.body, '_id');
+
+        assert.equal(res.body.issue_text, "newText");
+      });
+      chai.request(server)
+      .get('/api/issues/test')
+      .query({_id: '5fc4113e47c6e500b2053172'})
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        if (res.body.length > 0) {
+          assert.property(res.body[0].issue_text, "NewNewTextFromPut");
+          assert.property(res.body[0].open, false);
+        }
+        done();
+      });
     });
 
     test('No _id submitted => { error: "missing _id" }', function(done) {
+      chai.request(server)
+      .put('/api/issues/test')
+      .send({
+        issue_text: 'NewNewTextFromPut',
+        open: false
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        
+        assert.property(res.body, "error");
 
-      //done()
+        assert.equal(res.body.error, "missing _id");
+        done();
+      });
     });
 
     test('No fields to update => { error: "no update field(s) sent", _id: _id }', function(done) {
-      
-      //done()
+      chai.request(server)
+      .put('/api/issues/test')
+      .send({
+        _id: "5fc4113e47c6e500b2053172"
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        
+        assert.property(res.body, "error");
+        assert.property(res.body, "_id");
+
+        assert.equal(res.body.error, "no update field(s) sent");
+        assert.equal(res.body._id, "5fc4113e47c6e500b2053172");
+        done();
+      });
     });
 
     test('Invalid _id => { error: "missing _id" }', function(done) {
-      
-      //done();
+      chai.request(server)
+      .put('/api/issues/test')
+      .send({
+        _id: "5f665eb46e296f6b9b6a504d"
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        
+        assert.property(res.body, "error");
+
+        assert.equal(res.body.error, "missing _id");
+        done();
+      });
     });
     
   });
@@ -161,18 +298,75 @@ suite('Functional Tests', function() {
   suite('DELETE /api/issues/{project}', function() {
 
     test('Valid _id', function(done) {
-      
-      //done();
+      let validID = null;
+        chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Title',
+          issue_text: 'text',
+          created_by: 'Functional Test - Every field filled in'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+  
+          assert.isTrue(res.body.hasOwnProperty("_id"));
+
+          validID = res.body._id;
+      });
+
+      chai.request(server)
+      .delete('/api/issues/test')
+      .send({
+        _id: validID
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+
+        //{"result":"successfully deleted","_id":"5fc41c3947c6e500b205317e"}
+        assert.property(res.body, "result");
+        assert.property(res.body, "_id");
+
+        assert.equal(res.body.result, "successfully deleted");
+        assert.equal(res.body._id, validID);
+        done();
+      });
     });
+
     test('Invalid _id => { error: "could not delete", "_id": _id }', function(done) {
       const badId = "5f665eb46e296f6b9b6a504d";
-      
-      //done();
+      chai.request(server)
+      .delete('/api/issues/test')
+      .send({
+        _id: badId
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+
+        //{"error":"could not delete","_id":"5fc41c396e500b205317e"}
+        assert.property(res.body, "error");
+        assert.property(res.body, "_id");
+
+        assert.equal(res.body.result, "could not delete");
+        assert.equal(res.body._id, badId);
+        done();
+      });
     });
     
     test('No _id => { error: "missing _id" }', function(done) {
+      chai.request(server)
+      .delete('/api/issues/test')
+      .send({
+        noID: "123"
+      })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
 
-      //done();
+        //{ error: "missing _id" }
+        assert.property(res.body, "error");
+
+        assert.equal(res.body.result, "missing _id");
+        done();
+      });
     });
   });
 });
