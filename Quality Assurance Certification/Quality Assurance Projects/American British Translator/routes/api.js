b6bfb33a6ad76a75f70a7b4c0d90c224e8567f0e
Daniel G <dangar13@icloud.com>
2020-11-29 16:35:40 +0100

    Finished the API routing

--- Quality Assurance Certification/Quality Assurance Projects/American British Translator/routes/api.js
+++ Quality Assurance Certification/Quality Assurance Projects/American British Translator/routes/api.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
 
 const Translator = require('../components/translator.js');
@@ -17,5 +9,32 @@ module.exports = function (app) {
   app.route('/api/translate')
     .post((req, res) => {
       
+      if(isValidBody(req.body)) {
+        let result = {
+          "text": req.body.text,
+          "translation": translator.translate(req.body.text, req.body.locale == "american-to-british" ? true : false, true)
+        }
+        if (result.text == result.translation) {
+          result.translation = "Everything looks good to me!"
+        }
+        res.json(result);
+      } else {
+        res.json(isValidBody(req.body));
+      }
     });
 };
+
+// Return true if the body is valid.
+// Return a JSON object with the error otherwise.
+function isValidBody(body) {
+
+  if (!body.hasOwnProperty("text") || !body.hasOwnProperty("locale")) { 
+    return {"error": "Required field(s) missing"};
+  } else if (!["american-to-british", "british-to-american"].includes(body.locale)) {
+    return {"error": "Invalid value for locale field"};
+  } else if (body.text.trim().length === 0) {
+    return {"error": "No text to translate"};
+  } else {
+    return true;
+  }
+}
