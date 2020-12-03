160c39321f32b1b435c8ed22b3233260acac9e60
Daniel G <dangar13@icloud.com>
2020-12-04 00:34:00 +0100

    Finished the first HelmetJS challenges

--- Information Security Certification/Information Security with HelmetJS/myApp.js
+++ Information Security Certification/Information Security with HelmetJS/myApp.js
@@ -1,51 +1,42 @@
 var express = require('express');
 var app = express();
 
+let helmet = require("helmet");
 
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
+app.use(helmet.frameguard({ action: 'Deny' }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"]
+    }
+  })
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//The top-level helmet function is a wrapper around 11 smaller middlewares!
+// This...
+//app.use(helmet());
+// ...is equivalent to this:
+/*app.use(helmet.contentSecurityPolicy());
+  app.use(helmet.dnsPrefetchControl());
+  app.use(helmet.expectCt());
+  app.use(helmet.frameguard());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.hsts());
+  app.use(helmet.ieNoOpen());
+  app.use(helmet.noSniff());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.use(helmet.referrerPolicy());
+  app.use(helmet.xssFilter());
+*/
 
 module.exports = app;
 var api = require('./server.js');
