8d5f3f174a6c89aca77ade9be7e130671e8480df
Daniel G <dangar13@icloud.com>
2020-12-04 00:52:33 +0100

    Added Bcrypt base code

---
+++ Information Security Certification/Information Security with HelmetJS/Bcrypt/server.js
@@ -0,0 +1,43 @@
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const app         = express();
+fccTesting(app);
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, () => {});
