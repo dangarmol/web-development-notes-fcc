a3fed2ff760397976809c8d165b2f5f0d6962eed
Daniel G <dangar13@icloud.com>
2020-12-09 17:32:48 +0100

    Made some changes and renamed server

--- Information Security Certification/Python for Penetration Testing/tcp_server.py
+++ Information Security Certification/Python for Penetration Testing/tcp_server.py
@@ -2,6 +2,7 @@
 
 import socket
 
+# Creating the socket object
 server_socket = socket.socket(
    socket.AF_INET,  # This indicates we are using IPv4
    socket.SOCK_STREAM  # This indicates we are using a connection protocol (TCP), as UDP is not connection oriented.
@@ -14,14 +15,15 @@ port = 999
 server_socket.bind((host, port))
 # The OS may ask for admin permission when binding a port.
 
-server_socket.listen(5)  # Listen for incoming connections. We can set a maximum number of connections allowed (5).
+# Listen for incoming TCP connections. We can set a maximum number of connections allowed (5).
+server_socket.listen(5)
 
 while True:
    client_socket, address = server_socket.accept()
 
-   print("Received connection from: " %str(address))
+   print("Received connection from: %s" %str(address))
 
    server_message = "Hello, thanks for connecting to the server.\n"
-   client_socket.send(server_message)  # We send the message back to the client before closing connection.
+   client_socket.send(server_message.encode("ascii"))  # We send the message back to the client before closing connection.
 
    client_socket.close()
